5719c9c77ceccac3f51228ad324d15b05e49b241
Arnoldo Marin Ahijado <37191387+arnoldomarin@users.noreply.github.com>
2024-02-23 16:56:17 +0100

    added keycloack but not working

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,17 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import KeycloakProvider from "next-auth/providers/keycloak";
+
 export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
    providers: [
        GitHubProvider({
            clientId: process.env.GITHUB_ID,
            clientSecret: process.env.GITHUB_SECRET
        }),
+       KeycloakProvider({
+            clientId: process.env.KEYCLOAK_ID,
+            clientSecret: process.env.KEYCLOAK_SECRET,
+            issuer: process.env.KEYCLOAK_ISSUER,
+      })
    ],
 })
