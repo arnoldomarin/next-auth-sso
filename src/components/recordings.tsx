5a8cc4e114ca285711dbfb444881268470fb2f7b
Arnoldo Marin Ahijado <37191387+arnoldomarin@users.noreply.github.com>
2024-02-23 12:27:13 +0100

    clean up code

--- src/components/recordings.tsx
+++ src/components/recordings.tsx
@@ -5,19 +5,18 @@ import { useSession, signIn } from 'next-auth/react';
 
 const Recordings = () => {
     const session = useSession();
-    console.log(session);
-  return (
-        <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
-            <div>
-                {session.data?.user &&
-                    <h3>Recordings</h3>
-                }
-                {!session.data?.user &&
-                    <h3>Sign In to see Recordings</h3>
-                }
+    return (
+            <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
+                <div>
+                    {session.data?.user &&
+                        <h3>Recordings</h3>
+                    }
+                    {!session.data?.user &&
+                        <h3>Sign In to see Recordings</h3>
+                    }
+                </div>
             </div>
-        </div>
-  )
+    )
 }
 
 export default Recordings
